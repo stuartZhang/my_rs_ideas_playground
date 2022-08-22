0813ad025a1f6bfae47f0302e5c85fdf1df087d5
stuart_zhang <stuartpage@qq.com>
2022-08-23 07:58:47 +0800

    添加

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use ::derive_builder::Builder;
+#[derive(Builder)]
+struct Lorem {
+    ipsum: u32,
+}
 fn main() {
     println!("Hello, world!");
+}
+#[cfg(test)]
+mod tests {
+    use ::std::error::Error;
+    use super::{Lorem, LoremBuilder};
+    #[test]
+    fn test_lorem() -> Result<(), Box<dyn Error>> {
+        let x: Lorem = LoremBuilder::default().ipsum(42).build()?;
+        Ok(())
+    }
 }
