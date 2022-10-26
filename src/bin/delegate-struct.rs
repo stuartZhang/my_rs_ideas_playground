faf48ebad660fe8787b1356bcf18ec9a7191d799
stuart_zhang <stuartpage@qq.com>
2022-10-27 07:40:31 +0800

    再加一个例子。

--- src/bin/delegate-struct.rs
+++ src/bin/delegate-struct.rs
@@ -1,15 +1,18 @@
 /// 这是一个在【结构体·包装类型】`Wrapper`上，将部分【成员方法】委托给其
 /// 【字段值】或【字段值·表达式·返回值】的【成员方法】的例子。
 /// ---------------------------------------------------------------
+/// 使用场景：`Newtypes`设计模式
+/// ---------------------------------------------------------------
 /// `delegate crate`的优点：“以【成员成员】为最小委托单元，简单直观”，而
 /// 缺点：“不能让【结构体·包装类型】`Wrapper`自动实现只有【字段值】或【字
 /// 段值·表达式·返回值】才具备的`trait`”。
 /// ---------------------------------------------------------------
 /// 涉及到的功能点包括
 /// （1）修改·被代理成员方法·的【方法名】
-/// （2）修改·被代理成员方法·的【返回值类型】— 类型转换·需要·`From / TryFrom trait`实现。
-/// （3）忽略·被代理成员方法·的【返回值】
-/// （4）将【成员方法】委托于不同的【字段值】或【字段值·表达式·返回值】
+/// （2）修改·被代理成员方法·的【形参类型】— 类型转换·需要·`From / TryFrom trait`实现。
+/// （3）修改·被代理成员方法·的【返回值类型】— 类型转换·需要·`From / TryFrom trait`实现。
+/// （4）忽略·被代理成员方法·的【返回值】
+/// （5）将【成员方法】委托于不同的【字段值】或【字段值·表达式·返回值】
 mod data_structure {
     use ::delegate::delegate;
     use ::derive_builder::Builder;
@@ -22,24 +25,27 @@ mod data_structure {
     }
     impl Wrapper {
         delegate! {
-            to self.inner1 { // #4. 委托至【指定·字段】上的【成员方法】
+            to self.inner1 { // #5. 委托至【指定·字段】上的【成员方法】
                 #[call(push)] // #1. 修改·被代理成员方法名`push -> add`
                 pub fn add(&mut self, value: u32);
                 #[call(len)]
-                #[try_into(unwrap)] // #2. 修改·被代理成员方法·的返回值类型`usize -> u64`。
+                #[try_into(unwrap)] // #3. 修改·被代理成员方法·的返回值类型`usize -> u64`。
                                     // 利用`TryFrom trait`对【成员方法】的返回值做类型转换，
                                     // 并自动“拆箱”。若缺省元属性值`unwrap`，便需要明文手动
                                     // “拆箱”。
                 pub fn size(&self) -> u64;
-                // #3. 忽略·委托成员方法·的返回值。
+                // #4. 忽略·委托成员方法·的返回值。
                 pub fn pop(&mut self);
             }
-            to self.inner2.borrow_mut() { // #4. 委托至【指定·字段·表达式·返回值】上的【成员方法】
+            to self.inner2.borrow_mut() { // #5. 委托至【指定·字段·表达式·返回值】上的【成员方法】
                 #[call(push_str)] // #1. 修改·被代理成员方法名`push_str -> push`
                 pub fn push(&mut self, val: &str);
+                #[call(push)] // #2. 修改·被代理成员方法·的【形参类型】`u8 -> char`
+                              // 利用`From trait`对【成员方法】的【形参】做类型转换。
+                pub fn push_u8(&mut self, #[into] val: u8);
             }
             to self.size {
-                #[into] // #2. 修改·被代理成员方法·的返回值类型`i32 -> i64`。
+                #[into] // #3. 修改·被代理成员方法·的返回值类型`i32 -> i64`。
                         // 利用`From trait`对【成员方法】的返回值做类型转换。
                 pub fn pow(&self, exp: u32) -> i64;
             }
@@ -58,6 +64,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     // 经由【成员方法】的别名，调用【字段值】上的底层成员方法。
     wrapper.add(5);
     wrapper.push("abc");
+    wrapper.push_u8(200);
     // 修改被代理成员方法的返回值类型
     dbg!(wrapper.pow(2));
     dbg!(wrapper.size());
