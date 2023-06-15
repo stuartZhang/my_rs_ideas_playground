7b0a017685eb2bf49af5c2f022231645036e976e
stuart_zhang <stuartpage@qq.com>
2023-06-15 08:02:37 +0800

    行多态新例程

---
+++ src/bin/lens-row-polymorphism.rs
@@ -0,0 +1,45 @@
+#[path ="../utils.rs"]
+#[macro_use]
+mod utils;
+use ::std::error::Error;
+use ::lens_rs::{LensMut, LensRef, optics, Optics, Prism, PrismMut, PrismRef, Review, TraversalMut, TraversalRef};
+/**
+ * 所谓“行·多态”就是基于“鸭子类型”的多态化。即，只要数据结构（形状）相似，不管其类型命名是否一致，程序都将它们视作相
+ * 互兼容的（甚至同一）数据类型来处理。打趣地话，只要扁嘴·长蹼的活物都将被视作鸭子，即便它还可能是鸭嘴兽。
+ */
+fn main() -> Result<(), Box<dyn Error>> {
+    // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    { // 行多态·场景一：从同一个数据结构实例，根据不同的路径，从不同的位置，拾取/修改子数据结构。
+        let mut x: (i32, Result<(Vec<Option<(String, i32)>>, i32), ()>, Vec<i32>, Option<String>) = (
+            1,
+            Ok((
+                vec![
+                    Some(("a".to_string(), 2)),
+                    None,
+                    Some(("b".to_string(), 3)),
+                ],
+                4,
+            )),
+            vec![1, 2, 3],
+            None
+        );
+        let optics1: Optics![_2.[usize]] = optics!(_2.[1]);
+        // 在数据结构中，由路径寻找指向的目标值必须存在，因为“透镜`Lens`”路径。
+        fn must_have_i32<Ln, T: LensMut<Ln, i32>>(t: &mut T, ln: Ln) {
+            *t.view_mut(ln) += 1;
+        }
+        compare_log!(must_have_i32(&mut x, optics!(_0)); x);
+        compare_log!(must_have_i32(&mut x, optics1); x);
+        // 在数据结构中，由路径寻找指向的目标值既可存在也可不存在，因为“棱镜`Prism`”路径。
+        fn may_have_i32<Pm, T: PrismMut<Pm, i32>>(t: &mut T, pm: Pm) {
+            t.preview_mut(pm).map(|x| {
+                *x += 1
+            });
+        }
+        compare_log!(may_have_i32(&mut x, optics!(_1.Ok._1)); x);
+        compare_log!(may_have_i32(&mut x, optics1); x);
+    }
+    // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    Ok(())
+}
