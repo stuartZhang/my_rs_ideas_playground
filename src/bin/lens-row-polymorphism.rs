4d7e667cad68a1a1ca68f994aa94959aa12acbef
stuart_zhang <stuartpage@qq.com>
2023-06-19 08:11:04 +0800

    添加场景二的例程

--- src/bin/lens-row-polymorphism.rs
+++ src/bin/lens-row-polymorphism.rs
@@ -2,7 +2,7 @@
 #[macro_use]
 mod utils;
 use ::std::{error::Error, ops::AddAssign};
-use ::lens_rs::{LensMut, LensRef, optics, Optics, Prism, PrismMut, PrismRef, Review, TraversalMut, TraversalRef};
+use ::lens_rs::{Lens, LensMut, LensRef, optics, Optics, Prism, PrismMut, PrismRef, Review, TraversalMut, TraversalRef};
 /**
  * 所谓“行·多态”就是基于“鸭子类型”的多态化。即，只要数据结构（形状）相似，不管其类型命名是否一致，程序都将它们视作相
  * 互兼容的（甚至同一）数据类型来处理。打趣地话，只要扁嘴·长蹼的活物都将被视作鸭子，即便它还可能是鸭嘴兽。
@@ -25,8 +25,9 @@ fn main() -> Result<(), Box<dyn Error>> {
         );
         let optics1: Optics![_2.[usize]] = optics!(_2.[1]);
         // 在数据结构中，由路径指向的目标值必须存在，因为“透镜`Lens`”。
-        fn must_have_i32<P, V, T: LensMut<P, V>>(t: &mut T, lens: P)
-        where V: AddAssign<i32> {
+        fn must_have_i32<P, V, T>(t: &mut T, lens: P)
+        where V: AddAssign<i32>,
+              T: LensMut<P, V> {
             *t.view_mut(lens) += 1;
         }
         // - 路径目标一定存在
@@ -35,8 +36,9 @@ fn main() -> Result<(), Box<dyn Error>> {
         // - 路径目标可能不存在的情况是处理不了的，且会导致编译失败。
         // - 路径目标一定不存在的情况是处理不了的，且会导致编译失败。
         // 在数据结构中，由路径指向的目标值既可存在也可不存在，因为“棱镜`Prism`”。
-        fn may_have_i32<P, V, T: PrismMut<P, V>>(t: &mut T, prism: P)
-        where V: AddAssign<i32> {
+        fn may_have_i32<P, V, T>(t: &mut T, prism: P)
+        where V: AddAssign<i32>,
+              T: PrismMut<P, V> {
             t.preview_mut(prism).map(|x| {
                 *x += 1
             });
@@ -49,8 +51,9 @@ fn main() -> Result<(), Box<dyn Error>> {
         compare_log!(may_have_i32(&mut x, optics1); x);
         // - 路径目标一定不存在的情况是处理不了的，且会导致编译失败。
         // 在数据结构中，由路径寻找指向的目标值是一个集合，因为“棱镜`Traversal`”。
-        fn may_have_multi_i32<P, V, T: TraversalMut<P, V>>(t: &mut T, traversal: P)
-        where V: AddAssign<i32> {
+        fn may_have_multi_i32<P, V, T>(t: &mut T, traversal: P)
+        where V: AddAssign<i32>,
+              T: TraversalMut<P, V> {
             t.traverse_mut(traversal).into_iter().for_each(|x| { // 遍历每一个路径匹配项
                 *x += 1
             });
@@ -67,8 +70,37 @@ fn main() -> Result<(), Box<dyn Error>> {
         // - 路径目标一定不存在的情况是处理不了的，且会导致编译失败。
     }
     // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    {
-
+    { // 行多态·场景二：根据相同的路径，从不同类型（但形状相似）的数据结构实例，拾取/修改子数据结构。
+        #[derive(Lens)]
+        struct Foo<A, B> {
+            #[optic]
+            a: A,
+            #[optic]
+            b: B,
+        }
+        #[derive(Lens)]
+        struct Bar {
+            #[optic]
+            a: String,
+            #[optic]
+            c: i32,
+        }
+        // 上述定义的两个数据类型`Foo`与`Bar`有着相似的形状。即，它们都有相同的字段`a`。
+        let foo = (0, Foo {
+            a: "abc",
+            b: 12_f64
+        });
+        let bar = ("123", Bar {
+            a: "def".to_string(),
+            c: 12
+        });
+        // 使用“透镜”路径，要求目标字段在数据结构内必须存在。
+        fn with_field_a<V, T>(t: &T) -> &V
+        where T: LensRef<Optics![_1.a], V> {
+            t.view_ref(optics!(_1.a))
+        }
+        println!("{:29}{:?}", "Foo 数据结构内一定要存在 a 字段的【单值】", with_field_a(&foo));
+        println!("{:29}{:?}", "Bar 数据结构内一定要存在 a 字段的【单值】", with_field_a(&bar));
     }
     Ok(())
 }
