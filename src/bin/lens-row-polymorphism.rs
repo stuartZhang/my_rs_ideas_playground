262a6d5b9515827de8f20f7769210227fd4b7005
stuart_zhang <stuartpage@qq.com>
2023-07-06 07:36:24 +0800

    齐了。

--- src/bin/lens-row-polymorphism.rs
+++ src/bin/lens-row-polymorphism.rs
@@ -2,7 +2,7 @@
 #[macro_use]
 mod utils;
 use ::std::{error::Error, ops::AddAssign};
-use ::lens_rs::{Lens, LensMut, LensRef, optics, Optics, Prism, PrismMut, PrismRef, Review, TraversalMut, TraversalRef};
+use ::lens_rs::{Lens, LensMut, LensRef, optics, Optics, PrismMut, PrismRef, TraversalMut, TraversalRef};
 /**
  * 所谓“行·多态”就是基于“鸭子类型”的多态化。即，只要数据结构（形状）相似，不管其类型命名是否一致，程序都将它们视作相
  * 互兼容的（甚至同一）数据类型来处理。打趣地话，只要扁嘴·长蹼的活物都将被视作鸭子，即便它还可能是鸭嘴兽。
@@ -25,49 +25,49 @@ fn main() -> Result<(), Box<dyn Error>> {
         );
         let optics1: Optics![_2.[usize]] = optics!(_2.[1]);
         // 因为使用“透镜`Lens`”路径，由路径指向的目标值在数据结构内必须存在。
-        fn must_have_i32<P, V, T>(t: &mut T, lens: P)
-        where V: AddAssign<i32>,
+        fn must_have_value<P, V, T>(t: &mut T, lens: P, increment: V)
+        where V: AddAssign,
               T: LensMut<P, V> {
-            *t.view_mut(lens) += 1;
+            *t.view_mut(lens) += increment;
         }
         // - 路径目标一定存在
-        compare_log!(must_have_i32(&mut x, optics!(_0)); x);
-        compare_log!(must_have_i32(&mut x, optics1); x);
+        compare_log!(must_have_value(&mut x, optics!(_0), 1); x);
+        compare_log!(must_have_value(&mut x, optics1, 2); x);
         // - 路径目标可能不存在的情况是处理不了的，且会导致编译失败。
         // - 路径目标一定不存在的情况是处理不了的，且会导致编译失败。
         // 因为使用“棱镜`Prism`”路径，由路径指向的目标值在数据结构内允许不存在。
-        fn may_have_i32<P, V, T>(t: &mut T, prism: P)
-        where V: AddAssign<i32>,
+        fn may_have_value<P, V, T>(t: &mut T, prism: P, increment: V)
+        where V: AddAssign,
               T: PrismMut<P, V> {
             t.preview_mut(prism).map(|x| {
-                *x += 1
+                *x += increment
             });
         }
         // - 路径目标不存在
-        compare_log!(may_have_i32::<_, i32, _>(&mut x, optics!(_11)); x);
-        compare_log!(may_have_i32::<_, i32, _>(&mut x, optics!(_1.Ok._11)); x);
-        compare_log!(may_have_i32::<_, i32, _>(&mut x, optics!(_3.Ok._1)); x);
+        compare_log!(may_have_value(&mut x, optics!(_11), 1); x);
+        compare_log!(may_have_value(&mut x, optics!(_1.Ok._11), 2); x);
+        compare_log!(may_have_value(&mut x, optics!(_3.Ok._1), 3); x);
         // - 路径目标存在
-        compare_log!(may_have_i32(&mut x, optics!(_1.Ok._1)); x);
-        compare_log!(may_have_i32(&mut x, optics!(_0)); x);
-        compare_log!(may_have_i32(&mut x, optics1); x);
-        // 因为使用“棱镜`Traversal`”路径，由路径指向的目标值在数据结构内一定是集合，哪怕是单元素集合，甚至空集合。
-        fn may_have_multi_i32<P, V, T>(t: &mut T, traversal: P)
-        where V: AddAssign<i32>,
+        compare_log!(may_have_value(&mut x, optics!(_1.Ok._1), 4); x);
+        compare_log!(may_have_value(&mut x, optics!(_0), 5); x);
+        compare_log!(may_have_value(&mut x, optics1, 6); x);
+        // 因为使用“镜头`Traversal`”路径，由路径指向的目标值在数据结构内一定是集合，哪怕是单元素集合，甚至空集合。
+        fn may_have_multi_value<P, V, T>(t: &mut T, traversal: P, increment: V)
+        where V: AddAssign + Copy,
               T: TraversalMut<P, V> {
-            t.traverse_mut(traversal).into_iter().for_each(|x| { // 遍历每一个路径匹配项
-                *x += 1
-            });
+            for x in t.traverse_mut(traversal) { // 遍历每一个路径匹配项
+                *x += increment;
+            }
         }
         // - 路径目标可能存在多个或一个都没有，但不确定
-        compare_log!(may_have_multi_i32(&mut x, optics!(_1.Ok._0._mapped.Some._1)); x);
-        compare_log!(may_have_multi_i32(&mut x, optics!(_2._mapped)); x);
+        compare_log!(may_have_multi_value(&mut x, optics!(_1.Ok._0._mapped.Some._1), 1); x);
+        compare_log!(may_have_multi_value(&mut x, optics!(_2._mapped), 2); x);
         // - 路径目标可能存在，但不确定
-        compare_log!(may_have_multi_i32::<_, i32, _>(&mut x, optics!(_3.Ok._1)); x); // 之不存在
-        compare_log!(may_have_multi_i32(&mut x, optics!(_1.Ok._1)); x); // 之存在
+        compare_log!(may_have_multi_value::<_, i32, _>(&mut x, optics!(_3.Ok._1), 3); x); // 之不存在
+        compare_log!(may_have_multi_value(&mut x, optics!(_1.Ok._1), 4); x); // 之存在
         // - 路径目标一定存在
-        compare_log!(may_have_multi_i32(&mut x, optics!(_0)); x);
-        compare_log!(may_have_multi_i32(&mut x, optics1); x);
+        compare_log!(may_have_multi_value(&mut x, optics!(_0), 5); x);
+        compare_log!(may_have_multi_value(&mut x, optics1, 6); x);
         // - 路径目标一定不存在的情况是处理不了的，且会导致编译失败。
     }
     // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -107,8 +107,15 @@ fn main() -> Result<(), Box<dyn Error>> {
         where T: PrismRef<Optics![_1.c], V> {
             t.preview_ref(optics!(_1.c))
         }
-        println!("{:29}{:?}", "Foo 数据结构内不存在 a 字段", may_has_field_c::<i32, _>(&foo));
-        println!("{:29}{:?}", "Bar 数据结构内存在 a 字段", may_has_field_c(&bar));
+        println!("{:36}{:?}", "Foo 数据结构内不存在 c 字段", may_has_field_c::<i32, _>(&foo));
+        println!("{:37}{:?}", "Bar 数据结构内存在 c 字段", may_has_field_c(&bar));
+        // 因为使用“镜头`Traversal`”路径，由路径指向的目标值在数据结构内一定是集合，哪怕是单元素集合，甚至空集合。
+        fn may_have_multi_field_c<V, T>(t: &T) -> Vec<&V>
+        where T: TraversalRef<Optics![_1.c], V> {
+            t.traverse_ref(optics!(_1.c))
+        }
+        println!("{:36}{:?}", "Foo 数据结构内不存在 c 字段", may_have_multi_field_c::<i32, _>(&foo));
+        println!("{:37}{:?}", "Bar 数据结构内存在 c 字段", may_have_multi_field_c(&bar));
     }
     Ok(())
 }
