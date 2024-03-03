0b6c8648e1e4fc8ab56e5874ec92b108657ef3ec
stuart_zhang <stuartpage@qq.com>
2024-03-03 14:18:36 +0800

    纠正一处编译失败

--- src/bin/cacao-helloword.rs
+++ src/bin/cacao-helloword.rs
@@ -1,16 +1,19 @@
-use ::cacao::appkit::{App, AppDelegate, window::Window};
 
-#[derive(Default)]
-struct BasicApp {
-    window: Window
-}
-impl AppDelegate for BasicApp {
-    fn did_finish_launching(&self) {
-       self.window.set_minimum_content_size(400., 400.);
-       self.window.set_title("Hello World!");
-       self.window.show();
-    }
-}
 fn main() {
-    App::new("com.hello.world", BasicApp::default()).run();
+    #[cfg(target_os = "macos")]
+    {
+        use ::cacao::appkit::{App, AppDelegate, window::Window};
+        #[derive(Default)]
+        struct BasicApp {
+            window: Window
+        }
+        impl AppDelegate for BasicApp {
+            fn did_finish_launching(&self) {
+            self.window.set_minimum_content_size(400., 400.);
+            self.window.set_title("Hello World!");
+            self.window.show();
+            }
+        }
+        App::new("com.hello.world", BasicApp::default()).run();
+    }
 }
