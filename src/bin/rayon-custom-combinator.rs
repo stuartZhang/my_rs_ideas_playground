c045556f4024b56b007287343c2a6ba156b16e82
stuart_zhang <stuartpage@qq.com>
2024-12-03 07:44:33 +0800

    rayon 例程2

---
+++ src/bin/rayon-custom-combinator.rs
@@ -0,0 +1,18 @@
+
+use ::rayon::prelude::*;
+
+fn is_prime(n: u64) -> bool {
+    if n <= 1 {
+        return false;
+    }
+    (2..=(n as f64).sqrt() as u64).all(|i| n % i != 0)
+}
+
+fn main() {
+    let primes: Vec<u64> = (1..100000)
+        .into_par_iter()
+        .filter(|&x| is_prime(x))
+        .collect();
+
+    println!("Found {} primes", primes.len());
+}
--- src/bin/rayon-sum.rs
+++ src/bin/rayon-sum.rs
@@ -2,8 +2,6 @@ use ::rayon::prelude::*;
 
 fn main() {
     let numbers: Vec<i64> = (1..10001).collect();
-
     let sum: i64 = numbers.par_iter().sum();
-
     println!("Sum: {}", sum);
 }
