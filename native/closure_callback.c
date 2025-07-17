9a382d8ecaadc82c76fc35af6f40b8b91afbdfb9
stuart_zhang <stuartpage@qq.com>
2025-07-17 10:15:17 +0800

    新例程调通了

--- native/closure_callback.c
+++ native/closure_callback.c
@@ -1,7 +1,27 @@
-typedef void (*Callback)(int result, void *closure);
+#include <stddef.h>
 
-void add_two_numbers(int a, int b, Callback cb, void *closure)
+typedef void (*Callback1)(int result, void* closure);
+
+void add_two_numbers(int a, int b, Callback1 cb, void* closure)
 {
     int result = a + b;
     cb(result, closure);
+}
+
+typedef int (*Callback2)(int a);
+typedef int (*Callback3)(Callback2 cb2, int a);
+
+int callback2(int result)
+{
+    return result + result;
+}
+int register1(Callback3 cb3, int a)
+{
+    if (cb3 == NULL) {
+        return a;
+    }
+    if (a > 10) {
+        return cb3(NULL, a);
+    }
+    return cb3(callback2, a);
 }
---
+++ src/bin/nullable-pointer-optimization.rs
@@ -0,0 +1,25 @@
+use ::std::ffi::c_int;
+fn main() {
+    type Callback2 = extern "C" fn(c_int) -> c_int;
+    type Callback3 = extern "C" fn(Option<Callback2>, c_int) -> c_int;
+    // 导入 C 链接库的程序接口
+    extern "C" {
+        fn register1(cb: Option<Callback3>, a: c_int) -> c_int;
+    }
+    // 向 C 链接库导出程序接口
+    extern "C" fn callback3(process: Option<Callback2>, a: c_int) -> c_int {
+        match process {
+            Some(callback2) => callback2(a),
+            None => a * a
+        }
+    }
+    println!("Rust ➜ C 传值‘空函数指针’");
+    let result = unsafe { register1(None, 4) };
+    assert_eq!(result, 4);
+    println!("C ➜ Rust 回传‘空函数指针’");
+    let result = unsafe { register1(Some(callback3), 14) };
+    assert_eq!(result, 196);
+    println!("Rust ⮂ C 均传递‘非空函数指针’");
+    let result = unsafe { register1(Some(callback3), 4) };
+    assert_eq!(result, 8);
+}
